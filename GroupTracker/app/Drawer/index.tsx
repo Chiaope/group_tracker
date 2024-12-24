b877d00a5e1f6c8305780c6ef393346a4f06c2ba
Chiaope <gui_kang@hotmail.com>
2024-12-24 21:40:01 +0800

    added drawer for basic pages

---
+++ GroupTracker/app/Drawer/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@react-navigation/elements";
+import { View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Index() {
+    const navigation = useNavigation<any>();
+
+    return (
+        <>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+                <Button onPress={() => navigation.navigate('Expense')}>
+                    Go to Expense
+                </Button>
+            </View>
+        </>
+    );
+}
