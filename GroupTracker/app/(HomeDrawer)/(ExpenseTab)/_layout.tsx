5c76b95dcdceb5863c5aa96696f3132db018cc45
Chiaope <gui_kang@hotmail.com>
2025-04-10 16:26:22 +0800

    Updated icons

--- GroupTracker/app/(HomeDrawer)/(ExpenseTab)/_layout.tsx
+++ GroupTracker/app/(HomeDrawer)/(ExpenseTab)/_layout.tsx
@@ -1,17 +1,22 @@
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import { Tabs } from 'expo-router';
+import { View } from 'react-native';
 
 export default function ExpenseTabLayout(){
     return <Tabs screenOptions={{ headerShown: false }}>
         <Tabs.Screen name='ExpensePage' options={{
             tabBarIcon: () => {
-                return <Icon name='money-bills'/>
+                return <View style={{flex:1}}>
+                <Icon name='money-bill' size={25}/>
+            </View>
             },
             tabBarLabel: 'Expense'
         }}/>
         <Tabs.Screen name='ScheduledExpensePage' options={{
             tabBarIcon: () => {
-                return <Icon name='money-bill-transfer'/>
+                return <View style={{flex:1}}>
+                <Icon name='money-bill-transfer' size={25}/>
+            </View>
             },
             tabBarLabel: 'Scheduled Expense'
         }}/>
